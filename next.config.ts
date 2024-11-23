d1e25501a7320eab4e10ddbd173774bb6ea9678a
Matheus <matheus150101miranda@gmail.com>
2024-11-23 00:04:26 -0300

    feat: update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Ignora a dependência de 'canvas' no lado do servidor
+      config.externals.push("canvas")
+    } else {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+      }
+    }
+    return config
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
